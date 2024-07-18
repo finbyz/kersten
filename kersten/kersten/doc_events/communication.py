f36b2f28c33c59b6b7e6738e46cb92aa91c360e3
finbyz <info@finbyz.tech>
2024-07-18 10:12:52 +0000

    Remove Fixtures

--- kersten/kersten/doc_events/communication.py
+++ kersten/kersten/doc_events/communication.py
@@ -5,6 +5,6 @@ def on_update(self,method):
     create_opportunity(self)
 
 def create_opportunity(self):
-    if self.emial_account == 'Sweeprite Sweeping' and self.custom_updated == 0 and self.reference_doctype == 'Opportunity':
+    if self.email_account == 'Sweeprite Sweeping' and self.custom_updated == 0 and self.reference_doctype == 'Opportunity':
         frappe.db.set_value("Opportunity", self.reference_name, "opportunity_type", "Sweeping")
         self.db_set("custom_updated", 1)
