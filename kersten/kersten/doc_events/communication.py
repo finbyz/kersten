fca9d873fba8628602c705c4536656477bd2f3b4
finbyz <info@finbyz.tech>
2024-08-20 12:26:07 +0000

    OPPORTUNITY CHANGES MD

--- kersten/kersten/doc_events/communication.py
+++ kersten/kersten/doc_events/communication.py
@@ -6,5 +6,11 @@ def on_update(self,method):
 
 def create_opportunity(self):
     if self.email_account == 'Sweeprite Sweeping' and self.custom_updated == 0 and self.reference_doctype == 'Opportunity':
-        frappe.db.set_value("Opportunity", self.reference_name, "opportunity_type", "Sweeping")
+        assigned_to_docs = frappe.get_all("ToDo", filters={"reference_type": "Opportunity", "reference_name": self.reference_name}, fields=["name"])
+        for docs in assigned_to_docs:
+            frappe.delete_doc("ToDo", docs["name"])
+            frappe.db.commit()
+        doc = frappe.get_doc("Opportunity", self.reference_name)
+        doc.opportunity_type = "Sweeping"
+        doc.save()
         self.db_set("custom_updated", 1)
