d08bc9464b06c7bc3e3e1bb8c171b27ebfc53509
finbyz <info@finbyz.tech>
2024-10-17 05:46:21 +0000

    PS changes for blog

--- kersten/kersten/doctype/blogs/blogs.js
+++ kersten/kersten/doctype/blogs/blogs.js
@@ -1,8 +1,15 @@
 // Copyright (c) 2024, viral and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Blogs", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Blogs", {
+	refresh(frm) {
+        frm.set_query("blog_list", function(doc, cdt, cdn) {
+            var d = locals[cdt][cdn];
+            return {
+                "filters": [
+                    ["Blog Post", "published", "=", 1]
+                ]
+            };
+        });
+	},
+});
