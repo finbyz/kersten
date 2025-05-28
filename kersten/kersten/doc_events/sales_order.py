46b9c43408903f39fa5bbd8e8d297970ee425583
finbyz <info@finbyz.tech>
2025-05-28 04:14:09 +0000

    PS changes on code for comment

--- kersten/kersten/doc_events/sales_order.py
+++ kersten/kersten/doc_events/sales_order.py
@@ -5,7 +5,9 @@ from erpnext.crm.utils import (
 )
 
 def after_insert(self, method):
+    processed_docs = set()
     for row in self.get("items"):
-        if row.prevdoc_docname:
+        if row.prevdoc_docname and row.prevdoc_docname not in processed_docs:
             copy_comments("Quotation", row.prevdoc_docname, self)
             link_communications("Quotation", row.prevdoc_docname, self)
+            processed_docs.add(row.prevdoc_docname)
